aa25bb831f9c1d7de3b81d281f11d52d1bcbb693
Arielis <aetumasonis@gmail.com>
2020-11-13 16:30:39 +0100

    added new client

---
+++ client/src/pages/brand.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from 'react';
+import AlbumContainer from '../components/albumContainer.js';
+import { getAllAlbums } from '../lib/fetch-albums.js';
+
+const Home = () => {
+  const [albums, setAlbums] = useState([]);
+
+  const fetchAllAlbums = async () => {
+    const albumsData = await getAllAlbums();
+    setAlbums(albumsData);
+  };
+
+  useEffect(() => {
+    fetchAllAlbums();
+  }, []);
+
+  return (
+    <>
+      <main className="container-home">
+        <div className="left">
+          <h4>CHOOSE ALBUMS TO START YOUR FAVOURITE COLLECTION:</h4>
+        </div>
+        <div className="wrapper-album">
+          <AlbumContainer albums={albums} />
+        </div>
+        <div className="right">
+          <h4>GO TO COLLECTION</h4>
+          <a href="/collection" className="go">
+            &gt;
+          </a>
+        </div>
+      </main>
+    </>
+  );
+};
+export default Home;
+
