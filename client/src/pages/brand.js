f0ceac0582696445e1598093767567d0105c811e
Arielis <aetumasonis@gmail.com>
2020-11-16 00:31:33 +0100

    added one-brand-list.js

--- client/src/pages/brand.js
+++ client/src/pages/brand.js
@@ -1,47 +1,15 @@
 import React, { useEffect, useState } from 'react';
-import { getAllBrands } from '../lib/fetch-scrapper.js';
+import BrandsContainer from '../components/brands-container.js';
+import PhoneListByBrand from '../components/one-brand-list.js';
 
-const BrandsContainer = () => {
-  const [brands, setBrands] = useState([]);
 
-  const fetchAllBrands = async () => {
-    const brandsData = await getAllBrands();
-    console.log(brandsData);
-    setBrands(brandsData);
-  };
-
-  useEffect(() => {
-    fetchAllBrands();
-  }, []);
-
-/*     const handleClick = (event) => {
-    event.preventDefault();
-    const data = await getAllBrands();
-    setBrands(data);
-  };
- */
+const Brand = () => {
 
   return (
     <>
       <main className="container-brand">
         <section className="left">
-          <div className="wrapper-one">
-            {brands.map((brand) => {
-              console.log(brand);
-              return (
-                  <article
-                    key={brand._id}
-                   // onClick={handleClick}
-                    className="one-brand"
-                  >
-                    <div className="brand">
-                      <h4 className="one-brand-name">{brand.name}</h4>
-                    <p className="one-brand-devices">{`${ brand.devices } devices`}</p>
-                    </div>
-                  </article>
-              );
-            })}
-          </div>
+          <BrandsContainer />
         </section>
         <section className="right">
           <div className="wrapper-two">
@@ -53,11 +21,12 @@ const BrandsContainer = () => {
               eligendi quis ipsa, quod accusamus!
             </p>
           </div>
+          <PhoneListByBrand />
         </section>
       </main>
     </>
   );
 };
-export default BrandsContainer;
+export default Brand;
 
  
