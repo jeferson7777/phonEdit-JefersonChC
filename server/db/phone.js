39a6ccba3fa86d3c4388fbab09954ed66bc7551b
Arielis <aetumasonis@gmail.com>
2020-11-16 10:02:40 +0100

    added wrapper-three

--- server/db/phone.js
+++ server/db/phone.js
@@ -1,3 +1,11 @@
-phones = []
+const phonesByBrand = [
+  {
+    name: 'nova 8 SE',
+    img: 'https://fdn2.gsmarena.com/vv/bigpic/huawei-nova-8-se.jpg',
+    url: 'huawei_nova_8_se-10579.php',
+    description:
+      'Huawei nova 8 SE Android smartphone. Announced Nov 2020. Features 6.53″  display, MediaTek MT6853 Dimensity 720 5G chipset, 3800 mAh battery, 128 GB storage, 8 GB RAM.',
+  }
+];
 
-export default phones;
+export default phonesByBrand;
