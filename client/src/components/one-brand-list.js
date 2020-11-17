64ad1972c2dc8c3c43454a85c75320fb7557c3d4
Pedro Martin <pataruco@gmail.com>
2020-11-17 09:36:23 +0000

    Render a model with phone details

--- client/src/components/one-brand-list.js
+++ client/src/components/one-brand-list.js
@@ -4,11 +4,13 @@ import {
   getAllBrands,
   getPhoneDetails,
 } from '../lib/fetch-scrapper.js';
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
 
 const PhoneListByBrand = ({ id }) => {
   const [phonesByBrand, setphonesByBrand] = useState([]);
-  const [brands, setBrands] = useState([]);
-  const [phoneDetail, setPhoneDetail] = useState([]);
+  const [phoneDetail, setPhoneDetail] = useState(undefined);
   const phoneExample = {
     title: 'Huawei P40 lite 5G',
     img: 'https://fdn2.gsmarena.com/vv/bigpic/huawei-p40-lite-5g.jpg',
@@ -25,57 +27,74 @@ const PhoneListByBrand = ({ id }) => {
     },
   };
 
-  const [isClicked, setisClicked] = useState(false);
-
-    const fetchAllBrands = async () => {
-      const brandsData = await getAllBrands();
-      console.log(brandsData)
-      setBrands(brandsData);
-    };
-    useEffect(() => {
-      fetchAllBrands({});
-    }, []);
-
+  const [modalIsOpen, setIsOpen] = useState(false);
 
   const fetchPhonesByBrand = async (id) => {
     const phonesByBrandData = await getOneBrandById(id);
     setphonesByBrand(phonesByBrandData);
   };
 
-    const fetchPhoneDetails = async (id) => {
-      const phoneDetailData = await getPhoneDetails(id);
-      setPhoneDetail(phoneDetailData);
-    };
+  const fetchPhoneDetails = async (id) => {
+    const phoneDetailData = await getPhoneDetails(id);
+    console.log({ phoneDetailData });
+    setPhoneDetail(phoneDetailData);
+    setIsOpen(true);
+  };
 
-
-    const handlePhoneOnClick = (event) => {
-      event.preventDefault();
-      const element = event.currentTarget;
-      const id = element.getAttribute('href');
-      fetchPhoneDetails(id);
-      console.log(phoneDetail);
-
-      setisClicked(true);
-    };
-
+  const handlePhoneOnClick = async (event) => {
+    event.preventDefault();
+    const element = event.currentTarget;
+    const id = element.getAttribute('href');
+    await fetchPhoneDetails(id);
+  };
 
   useEffect(() => {
     fetchPhonesByBrand(id);
   }, [id]);
 
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
   return (
     <>
       <div className="wrapper-three">
         <div className="wrapper-titles">
-          <h3 className="brand-name"> {`${brands.name} PHONES`}</h3>
+          {/* <h3 className="brand-name"> {`${brands.name} PHONES`}</h3> */}
           <p className="brand-sub">
             Select the specific model you are looking for:
           </p>
         </div>
         <div className="wrapper-phone-items">
-          {isClicked && <p>{phoneExample.quick_spec.display_size}</p>}
+          <Modal isOpen={modalIsOpen} onRequestClose={closeModal}>
+            {modalIsOpen && (
+              <>
+                <button onClick={closeModal}>Close</button>
+                <section>
+                  <picture>
+                    <img src={phoneDetail.img} alt={phoneDetail.title} />
+                  </picture>
+                  <h3>{phoneDetail.title}</h3>
+                  <dl>
+                    {Object.entries(phoneDetail.quick_spec).map(
+                      ([key, value]) => {
+                        const formattedKey = key.replace('_', ' ');
+                        return (
+                          <div key={key}>
+                            <dt>{formattedKey}</dt>
+                            <dd>{value}</dd>
+                          </div>
+                        );
+                      },
+                    )}
+                  </dl>
+                </section>
+              </>
+            )}
+          </Modal>
           {phonesByBrand &&
             phonesByBrand.map((phone) => {
+              // console.log({ phone });
               return (
                 <div key={phone.name} className="item-container">
                   <h4>
@@ -100,3 +119,14 @@ const PhoneListByBrand = ({ id }) => {
   );
 };
 export default PhoneListByBrand;
+
+const specs = {
+  display_size: '6.0"',
+  display_res: '1080x1920 pixels',
+  camera_pixels: '13MP',
+  video_pixels: '1080p',
+  ram_size: '4GB RAM',
+  chipset: 'Helio P25',
+  battery_size: '4550mAh',
+  battery_type: 'Li-Po',
+};
